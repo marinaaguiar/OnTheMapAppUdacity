98479c233410b8f01db261401b2aff11d1f2afb2
Marina Aguiar <gordinho.marina@gmail.com>
2022-05-13 14:43:26 +0100

    add textfield reusable class

--- OnTheMapAppUdacity/Sources/Scenes/View/Field.swift
+++ OnTheMapAppUdacity/Sources/Scenes/View/Field.swift
@@ -1,8 +1,31 @@
-//
-//  Field.swift
-//  OnTheMapAppUdacity
-//
-//  Created by Marina Aguiar on 5/13/22.
-//
 
-import Foundation
+import UIKit
+
+class Field: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+        setLeftPaddingPoints(12)
+        setRightPaddingPoints(12)
+
+        backgroundColor = .white
+        layer.backgroundColor = UIColor(red: 0.962, green: 0.962, blue: 0.962, alpha: 1).cgColor
+        layer.cornerRadius = 15
+
+
+        func setLeftPaddingPoints(_ amount:CGFloat){
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+            self.leftView = paddingView
+            self.leftViewMode = .always
+        }
+        func setRightPaddingPoints(_ amount:CGFloat) {
+            let paddingView = UIView(frame: CGRect(x: 0, y: 0, width: amount, height: self.frame.size.height))
+            self.rightView = paddingView
+            self.rightViewMode = .always
+        }
+
+
+    }
+
+}
